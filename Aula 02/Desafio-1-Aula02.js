c5fde9d24dabfef6324ddb958e446c4c20edadb1
Pedro Duca <testandoduca@gmail.com>
2022-04-19 20:56:55 -0300

    removendo exercicio

--- Aula 02/Desafio-1-Aula02.js
+++ Aula 02/Desafio-1-Aula02.js
@@ -40,5 +40,3 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-
-// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
